9ad5f703cd741770de0b9fa719822c365d19a507
nicolas <nicolaser911@gmail.com>
2023-08-09 13:35:59 -0300

    creo los timeout en validaciones

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,15 @@
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
+    let tiempoFuera = setTimeout(()=>{
+        document.getElementById("alert-success").classList.remove("show");
+        clearTimeout(tiempoFuera);
+    }, 500);
 }
 
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
+    let tiempoFuera = setTimeout(()=>{
+        document.getElementById("alert-danger").classList.remove("show");
+        clearTimeout(tiempoFuera);
+    }, 500);
 }
