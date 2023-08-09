f4b66db96c75f8b583af947ebf13111b4bfaa37d
gonve13 <gonve13@gmail.com>
2023-08-09 13:42:05 -0300

    compara passwords

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,17 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+function comparePass(){
+    const pass1 = document.getElementById("password1");
+    const pass2 = document.getElementById("password2");
+
+    if (pass1.value !== pass2.value){
+        pass1.setCustomValidity("passwords no coinciden")
+        pass2.setCustomValidity("passwords no coinciden")
+    } else {
+        pass1.setCustomValidity("")
+        pass2.setCustomValidity("")
+    }
+}
